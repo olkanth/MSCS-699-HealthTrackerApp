67b58249ccdcf6c319d9ce6ff1845603ac87d13d
Laxmi Kanth Oruganti <oruganti.laxmikanth@gmail.com>
2026-07-09 21:50:27 -0500

    Helloworld endpoint

---
+++ main.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+
+app = FastAPI(title="HealthTracker App", swagger_ui_parameters={"syntaxHighlight": True})
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
